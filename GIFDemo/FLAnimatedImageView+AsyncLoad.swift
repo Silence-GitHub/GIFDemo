6c74fe6c7a153a15a79732b68c631222c52f1a56
Silence <lukaibolkb@163.com>
2017-04-20 16:12:52 +0800

    Fix static image bug

--- GIFDemo/FLAnimatedImageView+AsyncLoad.swift
+++ GIFDemo/FLAnimatedImageView+AsyncLoad.swift
@@ -12,24 +12,21 @@ extension FLAnimatedImageView {
     
     func setImage(with url: URL?, placeholderImage: UIImage?) {
         sd_internalSetImage(with: url, placeholderImage: placeholderImage, options: SDWebImageOptions(rawValue: 0), operationKey: nil, setImageBlock: { [weak self] (image, imageData) in
-            DispatchQueue.global(qos: .userInteractive).async { [weak self] in
-                guard self != nil else { return }
-                
-                let imageFormat = NSData.sd_imageFormat(forImageData: imageData)
-                if imageFormat == .GIF {
+            guard let strongSelf = self else { return }
+            
+            let imageFormat = NSData.sd_imageFormat(forImageData: imageData)
+            if imageFormat == .GIF {
+                DispatchQueue.global(qos: .userInteractive).async { [weak self] in
                     let animatedImage = FLAnimatedImage(animatedGIFData: imageData)
                     DispatchQueue.main.async { [weak self] in
                         guard let strongSelf = self else { return }
                         strongSelf.animatedImage = animatedImage
                         strongSelf.image = nil
                     }
-                } else {
-                    DispatchQueue.main.async { [weak self] in
-                        guard let strongSelf = self else { return }
-                        strongSelf.image = image
-                        strongSelf.animatedImage = nil
-                    }
                 }
+            } else {
+                strongSelf.image = image
+                strongSelf.animatedImage = nil
             }
             }, progress: nil, completed: nil)
     }
--- GIFDemo/TableViewController.swift
+++ GIFDemo/TableViewController.swift
@@ -45,7 +45,12 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! GIFCell
-        let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif")
+        let url: URL?
+        if indexPath.row % 2 == 0 {
+            url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif")
+        } else {
+            url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Faust_bei_der_Arbeit.JPG/440px-Faust_bei_der_Arbeit.JPG")
+        }
         if rightButton.isSelected {
             cell.gifView.setImage(with: url, placeholderImage: #imageLiteral(resourceName: "Loading"))
         } else {
